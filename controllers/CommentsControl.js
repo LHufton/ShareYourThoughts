e0319d4aaefd2f9ad299f97be270620e443f0c57
Luke Hufton <lukehufton@gmail.com>
2023-07-10 23:47:13 -0500

    posts and comments controllers

--- controllers/CommentsControl.js
+++ controllers/CommentsControl.js
@@ -1,8 +1,43 @@
-// const { Comments } = require('./models/comments')
+const Comment = require('../models/comment')
 
-// const GetComments = async(req, res) => {
-//   try {
-//     const comment = await Comments.find({})
-//     res.send(Comments)
-//   }
-// }
+const index = async (req, res) => {
+  console.log(req.query)
+  res.render('comments/index', {
+    comments: await Comment.find()
+  })
+}
+
+const show = async (req, res) => {
+  const comment = await Comment.findById(req.params.id)
+  res.render('comments/show', {})
+}
+
+const newComment = async (req, res) => {
+  res.render('comments/new', { title: 'New Comment', errorMsg: '' })
+}
+
+const create = async (req, res) => {
+  try {
+    const comment = await Comment.create(req.body)
+    res.redirect('/comments')
+  } catch (error) {
+    res.render('comments/new', { errorMsg: err.message })
+  }
+}
+
+const deleteComment = async (req, res) => {
+  try {
+    await Comment.findOneAndDelete({ _id: req.params.id })
+    res.redirect('/comments')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  index,
+  show,
+  new: newComment,
+  create,
+  delete: deleteComment
+}
---
+++ controllers/PostsControl.js
@@ -0,0 +1,43 @@
+const Post = require('../models/post')
+
+const index = async (req, res) => {
+  console.log(req.query)
+  res.render('posts/index', {
+    posts: await Post.find()
+  })
+}
+
+const show = async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('posts/show', {})
+}
+
+const newPost = async (req, res) => {
+  res.render('posts/new', { title: 'New Post', errorMsg: '' })
+}
+
+const create = async (req, res) => {
+  try {
+    const post = await Post.create(req.body)
+    res.redirect('/posts')
+  } catch (error) {
+    res.render('posts/new', { errorMsg: err.message })
+  }
+}
+
+const deletePost = async (req, res) => {
+  try {
+    await Post.findOneAndDelete({ _id: req.params.id })
+    res.redirect('/posts')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  index,
+  show,
+  new: newPost,
+  create,
+  delete: deletePost
+}
