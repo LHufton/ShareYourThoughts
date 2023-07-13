aea6134ac12e85072a0aa1115ea63563b944cec5
Luke Hufton <lukehufton@gmail.com>
2023-07-13 14:04:48 -0500

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Feed from './Components/Feed'
 import UserProfile from './pages/UserProfile'
 import Nav from './Components/Nav'
@@ -11,13 +10,18 @@ import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<UserProfile />} />
-      <Route path="/posts" element={<AllPosts />} />
-      <Route path="/comments" element={<AllComments />} />
-      <Route path="/feed" element={<Feed />} />
-    </Router>
+    <div className='"App'>
+      <Nav user={user} handleLogOut={handleLogOut} />
+      <main>
+        <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/posts" element={<AllPosts />} />
+          <Route path="/comments" element={<AllComments />} />
+          <Route path="/feed" element={<Feed />} />
+        </Router>
+      </main>
+    </div>
   )
 }
 
