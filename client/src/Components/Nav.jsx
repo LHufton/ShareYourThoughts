5fdceb2651839d7c54c65ce4ade937dfdf810d76
Luke Hufton <lukehufton@gmail.com>
2023-07-13 01:35:42 -0500

    nav updates

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleAuthClick = () => {
+    setIsLoggedIn(!isLoggedIn)
+  }
+
   return (
     <header>
       <div>
@@ -12,8 +19,26 @@ const Nav = () => {
             <NavLink to="/Comments">Comments</NavLink>
           </li>
           <li>
-            <NavLink to="/genreCard">Posts</NavLink>
+            <NavLink to="/Posts">Posts</NavLink>
           </li>
+          {!isLoggedIn ? (
+            <>
+              <li>
+                <NavLink to="/SignIn" onClick={handleAuthClick}>
+                  Sign In
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/Registration">Registration</NavLink>
+              </li>
+            </>
+          ) : (
+            <li>
+              <NavLink to="/SignOut" onClick={handleAuthClick}>
+                Sign Out
+              </NavLink>
+            </li>
+          )}
         </ul>
       </div>
     </header>
