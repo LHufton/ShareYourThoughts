433cd99bd587e983287f4518345f006293afa49b
Luke Hufton <lukehufton@gmail.com>
2023-07-14 00:19:11 -0500

    updated feed model

--- models/feed.js
+++ models/feed.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose')
-const feedSchema = require('./Feed')
+const { Schema, model } = require('mongoose')
+
+const postSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String }
+  },
+  { timestamps: true }
+)
+
+const commentSchema = new Schema({
+  title: { type: String, required: true },
+  body: { type: String }
+})
 
 const feedSchema = new Schema(
   {
     content: { type: String, required: true },
     type: { type: String, enum: ['post', 'comment'], required: true },
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    parent: { type: Schema.Types.ObjectId, ref: 'Feed' },
-    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+    parent: { type: Schema.Types.ObjectId, ref: 'Feed' }
   },
   { timestamps: true }
 )
