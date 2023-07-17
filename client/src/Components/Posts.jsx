6cc984917d34a491ab8d4c235a9b2868e2b7e3c8
Luke Hufton <lukehufton@gmail.com>
2023-07-17 16:03:29 -0500

    posts update function

--- client/src/Components/Posts.jsx
+++ client/src/Components/Posts.jsx
@@ -6,6 +6,9 @@ import Client from '../Services/api'
 const Post = (props) => {
   const [formValues, setFormValues] = useState({ content: '' })
   const [posts, setPosts] = useState([])
+
+  const [editPostContent, setEditPostContent] = useState('')
+  const [editingPost, setEditingPost] = useState(null)
   // const [date, setDate] = useState('none')
 
   // const onDateChange = (e) => {
@@ -18,12 +21,13 @@ const Post = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     console.log(formValues)
+    // Might need to add '/new' to this path.
     let response = await Client.post('/posts', formValues)
     setPosts([...posts, response.data])
     setFormValues({ content: '' })
   }
   const handleChange = (e) => {
-    setFormValues({ content: e.target.value })
+    setFormValues({ ...formValues, content: e.target.value })
   }
   useEffect(() => {
     console.log('Inside UseEffect')
@@ -36,7 +40,32 @@ const Post = (props) => {
     getPosts()
   }, [])
 
-  const handleDeletepost = async (id) => {
+  const handleChangeEdit = (e) => {
+    setEditPostContent(e.target.value)
+  }
+
+  const handleEdit = (id) => {
+    const postEdit = posts.find((post) => post._id === id)
+    setEditPostContent(id)
+    setEditingPost(postEdit.content)
+  }
+
+  const handledUpdatePost = async (id) => {
+    const updatedPost = {
+      ...posts.find((post) => post._id === id),
+      content: editPostContent
+    }
+    await Client.put(`/posts/${id}`, updatedPost)
+    setPosts(posts.map((post) => (post._id === id ? updatedPost : post)))
+    setEditingPost(null)
+    setEditPostContent('')
+  }
+
+  const handleDeletePost = async (id) => {
+    await Client.delete(`/posts/${id}`)
+    setPosts(posts.filter((post) => post._id !== id))
+  }
+  const handleUpdatePost = async (id) => {
     await Client.delete(`/posts/${id}`)
     setPosts(posts.filter((post) => post._id !== id))
   }
@@ -60,14 +89,24 @@ const Post = (props) => {
         {posts?.map((post) => (
           <div key={post._id}>
             <h4>{post.content}</h4>
-            <button onClick={() => handleDeletepost(post._id)}>
-              Delete Post
-            </button>
+            <button onClick={() => handleDeletePost(post._id)}>Delete</button>
+            <button onClick={() => handleEdit(post._id)}>Edit</button>
+            {editingPost === post._id && (
+              <div>
+                <textarea
+                  placeholder="Edit text"
+                  onChange={handleChangeEdit}
+                  value={editPostContent}
+                />
+                <button onClick={() => handleUpdatePost(post._id)}>
+                  Update
+                </button>
+              </div>
+            )}
           </div>
         ))}
       </section>
     </div>
   )
 }
-
 export default Post
