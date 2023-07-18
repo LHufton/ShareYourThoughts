f78b9e8036d4a43ec398d032ed8af6ceca009c85
Luke Hufton <lukehufton@gmail.com>
2023-07-18 15:09:56 -0500

    user model ref on posts and comments

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -5,22 +5,20 @@ const Nav = ({ user, handleLogOut }) => {
   let userOptions
   if (user) {
     userOptions = (
-      <nav>
+      <nav className="user-nav-links">
         <h3>Welcome {user.email}!</h3>
-        <div className="user-nav-links">
-          <Link to="/feed">Feed</Link>
-          <Link to="/Comments">Comments</Link>
-          <Link to="/Posts">Posts</Link>
-          <Link onClick={handleLogOut} to="/">
-            Sign Out
-          </Link>
-        </div>
+        <Link to="/feed">Feed</Link>
+        <Link to="/Comments">Comments</Link>
+        <Link to="/Posts">Posts</Link>
+        <Link onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
       </nav>
     )
   }
 
   const publicOptions = (
-    <nav>
+    <nav className="public-nav-links">
       <Link to="/">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/signin">Sign In</Link>
