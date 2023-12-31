4b5caa46372edc3418598e80555e17497c843709
LHufton <lukehufton@gmail.com>
2023-12-31 02:01:34 -0600

    proc file update

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,54 +1,29 @@
-const { Comment } = require('../models')
+const Comment = require('../models/Comment')
 
-const GetComments = async (req, res) => {
-  try {
-    const comments = await Comment.find({})
-    res.send(comments)
-  } catch (error) {
-    throw error
-  }
+export const GetComments = async (req, res) => {
+  const comments = await Comment.find({})
+  res.send(comments)
 }
 
-const CreateComment = async (req, res) => {
-  try {
-    const comment = await Comment.create({ ...req.body })
-    res.send(comment)
-  } catch (error) {
-    throw error
-  }
+export const CreateComment = async (req, res) => {
+  const comment = await Comment.create({ ...req.body })
+  res.send(comment)
 }
 
-const UpdateComment = async (req, res) => {
-  try {
-    const comment = await Comment.findByIdAndUpdate(
-      req.params.comment_id,
-      req.body,
-      {
-        new: true
-      }
-    )
-    res.send(comment)
-  } catch (error) {
-    throw error
-  }
+export const UpdateComment = async (req, res) => {
+  const comment = await Comment.findByIdAndUpdate(
+    req.params.comment_id,
+    req.body,
+    { new: true }
+  )
+  res.send(comment)
 }
 
-const DeleteComment = async (req, res) => {
-  try {
-    await Comment.deleteOne({ _id: req.params.comment_id })
-    res.send({
-      msg: 'Comment Deleted',
-      payload: req.params.comment_id,
-      status: 'Ok'
-    })
-  } catch (error) {
-    throw error
-  }
-}
-
-module.exports = {
-  GetComments,
-  CreateComment,
-  UpdateComment,
-  DeleteComment
+export const DeleteComment = async (req, res) => {
+  await Comment.deleteOne({ _id: req.params.comment_id })
+  res.send({
+    msg: 'Comment Deleted',
+    payload: req.params.comment_id,
+    status: 'Ok'
+  })
 }
