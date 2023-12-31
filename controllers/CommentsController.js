e4f8a8f9520bd861297a9cc699fc213ac573ce5a
LHufton <lukehufton@gmail.com>
2023-12-31 02:59:45 -0600

    module updates

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,16 +1,16 @@
 const Comment = require('../models/Comment')
 
-export const GetComments = async (req, res) => {
+const GetComments = async (req, res) => {
   const comments = await Comment.find({})
   res.send(comments)
 }
 
-export const CreateComment = async (req, res) => {
+const CreateComment = async (req, res) => {
   const comment = await Comment.create({ ...req.body })
   res.send(comment)
 }
 
-export const UpdateComment = async (req, res) => {
+const UpdateComment = async (req, res) => {
   const comment = await Comment.findByIdAndUpdate(
     req.params.comment_id,
     req.body,
@@ -19,11 +19,18 @@ export const UpdateComment = async (req, res) => {
   res.send(comment)
 }
 
-export const DeleteComment = async (req, res) => {
+const DeleteComment = async (req, res) => {
   await Comment.deleteOne({ _id: req.params.comment_id })
   res.send({
     msg: 'Comment Deleted',
     payload: req.params.comment_id,
     status: 'Ok'
   })
+}
+
+module.exports = {
+  GetComments,
+  CreateComment,
+  UpdateComment,
+  DeleteComment
 }
